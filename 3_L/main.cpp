b1dafbfe9e7302faa23819005cac951862d3bd8d
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-02-10 22:28:44 +0700

    update desciption

--- 3_L/main.cpp
+++ 3_L/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * @brief Demonstration of the Liskov substituation principle in OOP design.
+ * The Single Responsibility Principle (SRP) states that a class should have only one reason to change, 
+ * or in other words, it should have only one job or responsibility.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
